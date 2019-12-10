c42324e6bb5af85d5506ab413636871572a45fcf
chenkang98 <1045142602.qq.com>
2019-12-10 22:41:37 +0800

    first_commit

---
+++ js/main.js
@@ -0,0 +1,29 @@
+$(function () {
+   
+    $('.leftArea').click(function () { 
+        var n = $('.leftArea ').index(this);
+        $('.rightArea').removeClass('selected');
+        $('.rightArea').eq(n).addClass('selected');;
+        $('.leftArea').removeClass('select');
+        $(this).addClass('select');
+    });
+   
+    $('#download').click(function () {
+          
+          $('.file').each(function (index, element) {
+               // element == this
+                var filename = $('.file').eq(index).children('.filename').text();
+                filename=filename.trim();
+                filename= "DownLoad?"+filename
+                 
+                $('.file').eq(index).children().filter('a').attr("href", filename);
+           });
+    });
+    $('.record').mouseenter(function () { 
+        this.children().css("color","#09aaff");
+    });
+    $('.record').mouseleave(function () { 
+        this.children().css("color", "blank");
+    });
+});
+ 
